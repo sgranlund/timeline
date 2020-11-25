bffb314576a33071870461508d07fa4fe41c9e2f
Sebastian Granlund <sgran@kth.se>
2020-11-25 17:19:53 +0100

    added draggable list

--- src/Presenters/gameBoard.js
+++ src/Presenters/gameBoard.js
@@ -3,24 +3,36 @@ import { GameBoardView } from "../Views/gameBoardView.js";
 
 //const h = React.createElement;
 
-const finalSpaceCharacters = [
+const myCharacters = [
   {
-    id: 'abra',
-    name: 'Abraham',
+    id: "abra",
+    name: "Abraham",
   },
   {
-      id: 'seb',
-      name: 'Sebastian',
-    },
+    id: "seb",
+    name: "Sebastian",
+  },
   {
-      id: 'hej',
-      name: 'Hallå'
-  }
-]
-
-const [characters, updateCharacters] = React.useState(finalSpaceCharacters);
-
+    id: "hej",
+    name: "Hallå",
+  },
+];
 
 export function GameBoard() {
-  return <GameBoardView />;
+  const [characters, updateCharacters] = React.useState(myCharacters);
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    console.log(result);
+    const items = Array.from(myCharacters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
+  }
+  return (
+    <GameBoardView
+      myCharacters={characters}
+      handleOnDragEnd={handleOnDragEnd}
+    />
+  );
 }
--- src/Views/gameBoardView.js
+++ src/Views/gameBoardView.js
@@ -1,38 +1,41 @@
-import React, { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-
-function handleOnDragEnd(result) {
-    console.log(result)
-}
-
-export const GameBoardView = ({}) => (
+export const GameBoardView = ({ myCharacters, handleOnDragEnd }) => {
+  return (
     <div>
-        <div>
-            <button>Save game</button>
-        </div>
-        <div>
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId="characters">
-                {(provided) => (
-        <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-            {finalSpaceCharacters.map(({id, name}, index) => {
-        return (
-            <Draggable key={id} draggableId={id} index={index}>
+      <div>
+        <button>Save game</button>
+      </div>
+      <div>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="characters">
             {(provided) => (
-        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-        <p>
-          { name }
-        </p>
-      </li>
-       )}
-       </Draggable>
-    );
-  })}
-  {provided.placeholder}
-</ul>)}
-</Droppable>
-</DragDropContext>
-        </div>
+              <ul
+                className="characters"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {myCharacters.map(({ id, name }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <p>{name}</p>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </div>
-)
+  );
+};
