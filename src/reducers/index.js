097fd78b24930be021b32e941c084111da168abd
Emilia Haltorp <haltorp@kth.se>
2020-12-08 15:47:01 +0100

    fetch new master

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import counter from "./counter";
 import yearReducer from "./yearReducer";
 import nameReducer from "./nameReducer";
+import pointsReducer from "./points";
 import { combineReducers } from 'redux';
 
 const allReducers = combineReducers({
     counter,
     years: yearReducer,
-    names: nameReducer
+    names: nameReducer,
+    points: pointsReducer
 });
 
 export default allReducers;
