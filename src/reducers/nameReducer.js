b65560446e939982ede86a9f81f0ee98d061c473
Emilia Haltorp <haltorp@kth.se>
2020-12-08 11:17:21 +0100

    initialize name

--- src/reducers/nameReducer.js
+++ src/reducers/nameReducer.js
@@ -1,4 +1,4 @@
-const nameReducer = (state="", action) => {
+const nameReducer = (state={name1: "seb", name2: "em"}, action) => {
     switch (action.type) {
         case 'NAME1':
             return {
