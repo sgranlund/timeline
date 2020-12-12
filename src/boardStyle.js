a957ece2980a77cf8d82b6a04a00783e8a7690c9
Sebastian Granlund <sgran@kth.se>
2020-12-12 18:16:35 +0100

    make card show onDrag

--- src/boardStyle.js
+++ src/boardStyle.js
@@ -1,7 +1,7 @@
 //---------------Styling start---------------//
 const grid = 8;
 
-export const getItemStyle = (turn, isDragging, draggableStyle) => ({
+export const getItemStyle = (showcard, turn, isDragging, draggableStyle) => ({
 	// some basic styles to make the items look a bit nicer
 	userSelect: "none",
 
@@ -11,29 +11,8 @@ export const getItemStyle = (turn, isDragging, draggableStyle) => ({
 		"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
 	// change background colour if dragging
 	borderColor: isDragging ? "lightgreen" : "black",
-
-	backgroundColor: turn ? "darkgray" : "white",
-	color: turn ? "#696969" : "black",
-
-	
-	//backgroundColor: newCard ? "black" : "white",
-	// styles we need to apply on draggables
-	...draggableStyle,
-});
-
-export const noViewCard = (mouse, isDragging, draggableStyle) => ({
-	userSelect: "none",
-
-	margin: `0 ${grid / 2}px 0 ${grid / 2}px`,
-	//fontSize: "11px",
-	fontFamily:
-		"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-	// change background colour if dragging
-	borderColor: isDragging ? "lightgreen" : "black",
-
-	backgroundColor: mouse ?  "white" : "black",
-	//color: newCard ? "#696969" : "black",
-
+	color: turn ? "#B8B8B8" : "black",
+	backgroundColor: showcard && !isDragging ? "black" : "white",
 	// styles we need to apply on draggables
 	...draggableStyle,
 });
@@ -41,7 +20,6 @@ export const noViewCard = (mouse, isDragging, draggableStyle) => ({
 export const getListStyle = (isDraggingOver) => ({
 	//borderBottom: "10px solid black",
 
-	borderColor: isDraggingOver ? "lightgreen" : "black",
 	display: "flex",
 	padding: grid,
 	overflow: "auto",
